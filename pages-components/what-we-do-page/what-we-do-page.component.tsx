b8522b7d432807f8bb354766d1a074ade4ab2936
Сергей Шабанов <shsv382@gmail.com>
2022-06-27 09:09:01 +0300

    Services page styled

--- pages-components/what-we-do-page/what-we-do-page.component.tsx
+++ pages-components/what-we-do-page/what-we-do-page.component.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './what-we-do-page.module.scss';
+import wwdStyles from './what-we-do-page.module.scss';
 import Link from 'next/link';
 import MainArticle from '../../components/main-article/main-article.component';
 
@@ -14,42 +14,44 @@ const WhatWeDoPage: React.FC<WhatWeDoTypes> = ({ introdution }) => {
     const [appearancable, setAppearancable] = useState(true);
     useEffect(() => setAppearancable(true), [service])
     return (
-        <div className='what-we-do-page'>
-            <nav className="services-navigation">
-                {
-                    introdution.map(s => (
-                        <span 
-                            key={`nav-${s.header}`} 
-                            className={`link services-navigation-item ${(service.toLowerCase() === s.header.toLowerCase()) && `services-navigation-item-checked`}`} 
-                            onClick={() => {
-                                setAppearancable(false);
-                                setTimeout(() => {
-                                    setAppearancable(true);
-                                    setService(s.header.toLowerCase())
-                                }, 5)
-                                // setService(s)
-                            }
-                            }>
-                            {s.header[0].toUpperCase() + s.header.slice(1).toLowerCase()}
-                        </span>)
-                    )
-                }
-            </nav>
-            <div className={
-                `what-we-do-page__content 
-                ${appearancable ? "appearancable" : ""}`
-            }>
-                <MainArticle 
-                    imageURL={ introdution.filter(item => item.header.toLowerCase() === service)[0].imageURL }
-                    header={ introdution.filter(item => item.header.toLowerCase() === service)[0].header }
-                    description={ introdution.filter(item => item.header.toLowerCase() === service)[0].description }
-                >
-                <Link href={service.toLowerCase()}>
-                    <a className="button button-secondary button-secondary-default" style={{marginBottom: 30}}>
-                        Read More
-                    </a>
-                </Link>
-                </MainArticle>
+        <div className={`${wwdStyles.servicesPage}`}>
+            <div className={`${wwdStyles.whatWeDoPage}`}>
+                <nav className={`${wwdStyles.servicesNavigation}`}>
+                    {
+                        introdution.map(s => (
+                            <span 
+                                key={`nav-${s.header}`} 
+                                className={`link ${wwdStyles.servicesNavigationItem} ${(service.toLowerCase() === s.header.toLowerCase()) && wwdStyles.servicesNavigationItemChecked}`} 
+                                onClick={() => {
+                                    setAppearancable(false);
+                                    setTimeout(() => {
+                                        setAppearancable(true);
+                                        setService(s.header.toLowerCase())
+                                    }, 5)
+                                    // setService(s)
+                                }
+                                }>
+                                {s.header[0].toUpperCase() + s.header.slice(1).toLowerCase()}
+                            </span>)
+                        )
+                    }
+                </nav>
+                <div className={
+                    `${wwdStyles.whatWeDoPage__content} 
+                    ${appearancable ? "appearancable" : ""}`
+                }>
+                    <MainArticle 
+                        imageURL={ introdution.filter(item => item.header.toLowerCase() === service)[0].imageURL }
+                        header={ introdution.filter(item => item.header.toLowerCase() === service)[0].header }
+                        description={ introdution.filter(item => item.header.toLowerCase() === service)[0].description }
+                    >
+                    <Link href={service.toLowerCase()}>
+                        <a className="button button-secondary button-secondary-default" style={{marginBottom: 30}}>
+                            Read More
+                        </a>
+                    </Link>
+                    </MainArticle>
+                </div>
             </div>
         </div>
     );
