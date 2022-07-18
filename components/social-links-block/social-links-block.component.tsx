44e1e6ea4c96619aa560e7afab5bbe0caaef28d3
Сергей Шабанов <shsv382@gmail.com>
2022-07-18 13:08:43 +0300

    Added twitter and linkedin icons

--- components/social-links-block/social-links-block.component.tsx
+++ components/social-links-block/social-links-block.component.tsx
@@ -1,6 +1,7 @@
 import slbStyles from './social-links-block.module.scss';
 import SocialLink from '../social-link/social-link.component';
 import { SocialNetworksTypes } from '../../data/content';
+import Image from 'next/image';
 
 interface SocialLinksBlockTypes {
     socialNetworks: SocialNetworksTypes
@@ -10,11 +11,15 @@ export default function SocialLinksBlock({ socialNetworks }: SocialLinksBlockTyp
     return (
         <div className={`${slbStyles.socialLinksBlock}`}>
             { socialNetworks.linkedIn ? 
-                <SocialLink url={`https://www.linkedin.com/in/${socialNetworks.linkedIn}`}>in</SocialLink> :
+                <SocialLink url={`https://www.linkedin.com/in/${socialNetworks.linkedIn}`}>
+                    <Image src="/icons/linkedin.svg" width="20" height="20" alt="linkedIn" />
+                </SocialLink> :
                 null
             }
             { socialNetworks.twitter ? 
-                <SocialLink url={`https://twitter.com/${socialNetworks.twitter}`}>t</SocialLink> :
+                <SocialLink url={`https://twitter.com/${socialNetworks.twitter}`}>
+                    <Image src="/icons/twitter.svg" width="20" height="20" alt="twitter" />
+                </SocialLink> :
                 null
             }
         </div>
