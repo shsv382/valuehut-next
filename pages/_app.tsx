ddebec9c2f426e5f002a8445f145061acda489ee
Сергей Шабанов <shsv382@gmail.com>
2022-08-03 08:05:03 +0300

    Added Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app';
-import Layout from '../components/layout/layout.component.jsx'
+import Layout from '../components/layout/layout.component.jsx';
+import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageViews();
+
   return (
     <Layout>
+      <GoogleAnalytics />
       <Component {...pageProps} />
     </Layout>
   ) 
